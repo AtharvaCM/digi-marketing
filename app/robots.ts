765ddff032bfa9f257f2d3c68f8d040daf5aca95
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2025-03-08 11:25:56 +0530

    fix: robots file - disallow on preview environments

--- app/robots.ts
+++ app/robots.ts
@@ -1,8 +1,22 @@
 import { MetadataRoute } from 'next';
 
-import { PROD_BASE_URL } from '@/utils/constants';
+import { BASE_URL, PROD_BASE_URL } from '@/utils/constants';
 
 export default function robots(): MetadataRoute.Robots {
+  const isPreview = process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview' && PROD_BASE_URL !== BASE_URL;
+
+  if (isPreview) {
+    // If in a preview environment, disallow all
+    // so that bots won't index the preview enviromnent pages.
+    return {
+      rules: {
+        userAgent: '*',
+        disallow: '/',
+      },
+      sitemap: '', // Omit the sitemap as well
+    };
+  }
+
   return {
     rules: {
       userAgent: '*',
