11c82c300f55d1680a4fab0b1a798f2ee333309a
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2025-03-15 18:21:45 +0530

    refactor: remove actions file for get page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,10 @@
 import { NextPage } from 'next';
+import { groq } from 'next-sanity';
 
 import Modules from '@/components/modules';
+import { sanityFetch } from '@/sanity/lib/fetch';
+import { modulesQuery } from '@/sanity/lib/queries';
 import processMetadata from '@/utils/functions/process-metadata';
-
-import { getPage } from './actions';
 
 export async function generateMetadata() {
   const page = await getPage();
@@ -17,3 +18,47 @@ const Page: NextPage = async () => {
 };
 
 export default Page;
+
+// =====================================
+// Helper function
+// =====================================
+
+async function getPage() {
+  const page = await sanityFetch<Sanity.Page>({
+    query: groq`*[_type == 'page' && metadata.slug.current == 'index'][0]{
+      ...,
+      modules[]{ ${modulesQuery} },
+      metadata {
+        title,
+        description,
+        keywords,
+        applicationName,
+        referrer,
+        canonical,
+        authors[] {
+          name,
+          "url": authorUrl
+        },
+        openGraph {
+          title,
+          description,
+          type,
+          "url": canonical,
+          siteName,
+          images[] {
+            "url": image.asset->url + '?w=1200',
+            width,
+            height,
+            alt
+          }
+        }
+      }
+    }`,
+    params: {},
+    tags: ['homepage'],
+  });
+
+  if (!page) throw new Error('Missing "page" document with metadata.slug "index" in Sanity Studio');
+
+  return page;
+}
